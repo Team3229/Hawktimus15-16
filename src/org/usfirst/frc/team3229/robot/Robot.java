2d598f6d8ca0b6137e69bde6f9503d16c4e7f80b
DoctorMooch <amhyder5@gmail.com>
2016-02-13 16:00:12 -0500

    change joystick to contain 1 ps3 controller

--- src/org/usfirst/frc/team3229/robot/Robot.java
+++ src/org/usfirst/frc/team3229/robot/Robot.java
@@ -19,24 +19,24 @@ import edu.wpi.first.wpilibj.Timer;
 public class Robot extends SampleRobot {
     RobotDrive myRobot;  // class that handles basic drive operations
     //Use two joysticks for two sticks on controller (one for left tread, one for right)
-    Joystick leftStick;
-    Joystick rightStick;
+    Joystick ps3;
+
+    double leftX = ps3.getRawAxis(0);
+    double leftY = ps3.getRawAxis(1);
+    double rightX = ps3.getRawAxis(4);
+    double rightY = ps3.getRawAxis(5);
+    
     public Robot() {
         myRobot = new RobotDrive(0, 1, 2, 3); //Four arguments are present because 2 motors per tread (2*2=4)
         myRobot.setExpiration(0.1);
         //Map sticks
-        leftStick = new Joystick(0);
-        rightStick = new Joystick(1);
+        ps3 = new Joystick(0);
     }
-
-    
-    /**
-     * Runs the motors with tank steering.
-     */
+    //Runs the motors with tank steering.
     public void operatorControl() {
         myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) { //Start loop that will be run while game is active
-        	myRobot.tankDrive(leftStick, rightStick);
+        	myRobot.tankDrive(ps3,ps3);
             Timer.delay(0.005);		// wait for a motor update time
         }
     }
